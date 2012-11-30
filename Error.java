9b8321c10525b3b3b784f3b0786915932ce6f310
Charlie Brodie <charliebrodie@Charlie-Brodies-MacBook-Pro.local>
2012-11-30 19:25:40 +0000

    I have now completed part a) of the exercise

--- Error.java
+++ Error.java
@@ -2,18 +2,23 @@ import java.util.*;
 
 public class Error{
 
-public void go(int first, int second, int third) { 
+public void mean(){ 
+	int total = 0;
+	int i = 0;
 
-		List<Integer> theList = new ArrayList<Integer>(2);
-		theList.add(first);
-		theList.add(second);
+	do{
+		System.out.println("Please enter a number");
+		String str = System.console().readLine();
+		int num = Integer.parseInt(str);
+		total += num;
+		i++;
 
-		try{
-			theList.add(third);
-		} catch (Exception ex){
-			ex.printStackTrace();
-		} catch (NullPointerException ex){
-			ex.printStackTrace();
-		}
+	}while (i <= 10);
+
+	int result = total/10;
+	System.out.println("");
+
+	System.out.println("Thank you, the mean of the numbers you entered was " + result);
+
 	}
 }
