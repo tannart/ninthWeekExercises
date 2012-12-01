5fd3f9bc2ae7de83b8e35fd9a0f96b196a8f5427
Charlie Brodie <charliebrodie@Charlie-Brodies-MacBook-Pro.local>
2012-12-01 13:23:38 +0000

    I have now completed this exercise with full functionality.

---
+++ PatientTest.java
@@ -0,0 +1,16 @@
+public class PatientTest{
+	
+    public static void main(String[] args){
+	
+    	Patient tester = new Patient("Jim", 78);
+
+    	tester.getDetails();
+    	tester.getDetails();
+    	tester.getDetails();
+    	tester.getDetails();
+    	tester.getDetails();
+
+    	tester.showAll();
+
+	}
+}	
